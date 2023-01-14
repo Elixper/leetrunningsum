9c1bb805ccdfbc15924170affb282a01bf1a14e4
Ludwig Pereira <ludwig.m.pereira@gmail.com>
2023-01-14 16:41:04 +0100

    new test

--- runningsum.js
+++ runningsum.js
@@ -4,10 +4,13 @@
  */
  var runningSum = function(nums) {
   let arr = []
-  while(arr.length <= nums.length+1){
+  let final = nums[0]
+  while(arr.length <= nums.length){
   arr.unshift(nums.reduce((acc,curr)=>(acc+curr)));
+  console.log(arr)
+  nums.pop();
   }
-  arr.unshift(nums[0])
+  arr.unshift(final)
   return arr
 };
 
@@ -15,3 +18,15 @@
 // Output: [1,3,6,10]
 console.log(runningSum([1,2,3,4]))
 // Explanation: Running sum is obtained as follows: [1, 1+2, 1+2+3, 1+2+3+4].
+
+console.log(runningSum([2,3,1]))
+
+// var runningSum = function(nums) {
+//   let arr = []
+//   while(arr.length <= nums.length+1){
+//   arr.unshift(nums.reduce((acc,curr)=>(acc+curr)));
+//   nums.pop();
+//   }
+//   arr.unshift(nums[0])
+//   return arr
+// };
